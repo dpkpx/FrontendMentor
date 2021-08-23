f9d4086cc93cfd8d988dda87447574f4a532e58f
dpk <contactdpk1@gmail.com>
2021-08-23 17:55:55 +0530

    upload

---
+++ article-preview-component-master/dist/script.js
@@ -0,0 +1,2 @@
+"use strict";function active(e){var o=document.querySelector(".card--active").style;o.display="flex",o.position="absolute"}console.log("hello");
+//# sourceMappingURL=script.js.map
---
+++ article-preview-component-master/src/js/script.js
@@ -0,0 +1,9 @@
+
+console.log('hello')
+
+function active(object){
+ 
+    const style = document.querySelector('.card--active').style;
+    style.display="flex";
+    style.position="absolute";
+}
